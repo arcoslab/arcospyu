b3015f9697eb49e5f8ac5146cf32a81b96c1a4bb
daniel garcia vaglio <degv364@gmail.com>
2016-04-16 00:15:08 +0000

    Add more available keys to rawkey.py

--- arcospyu/rawkey/rawkey.py
+++ arcospyu/rawkey/rawkey.py
@@ -38,7 +38,7 @@ class Raw_key():
             out.append(os.read(self.fd,1))
         while self.new_data(0.0):
             out.append(os.read(self.fd,1))
-            
+        print out   
         return(out)
 
     def get_num_chars(self,timeout=0):
@@ -48,13 +48,73 @@ class Raw_key():
         termios.tcsetattr(self.fd,termios.TCSAFLUSH,self.old)
 
 class Keys():
+    #this is ASCII
+    UP_ARROW=   [27, 91, 65]
+    DOWN_ARROW= [27, 91, 66]
     RIGHT_ARROW=[27, 91, 67]
-    LEFT_ARROW=[27, 91, 68]
-    UP_ARROW=[27, 91, 65]
-    DOWN_ARROW=[27, 91, 66]
+    LEFT_ARROW= [27, 91, 68]
+    number0=[48]
+    number1=[49]
+    number2=[51]
+    number3=[52]
+    number4=[53]
+    number5=[54]
+    number6=[55]
+    number7=[56]
+    number8=[57]
+    number9=[58]
+    cap_a=[65]
+    cap_b=[66]
+    cap_c=[67]
+    cap_d=[68]
+    cap_e=[69]
+    cap_f=[70]
+    cap_g=[71]
+    cap_h=[72]
+    cap_i=[73]
+    cap_j=[74]
+    cap_k=[75]
+    cap_l=[76]
+    cap_m=[77]
+    cap_n=[78]
+    cap_o=[79]
+    cap_p=[80]
+    cap_q=[81]
+    cap_r=[82]
+    cap_s=[83]
+    cap_t=[84]
+    cap_u=[85]
+    cap_v=[86]
+    cap_w=[87]
+    cap_x=[88]
+    cap_y=[89]
+    cap_z=[90]
+    a=[97]
+    b=[98]
+    c=[99]
     d=[100]
+    e=[101]
     f=[102]
     g=[103]
+    h=[104]
+    i=[105]
+    j=[106]
+    k=[107]
+    l=[108]
+    m=[109]
+    n=[110]
+    o=[111]
+    p=[112]
+    q=[113]
+    r=[114]
+    s=[115]
+    t=[116]
+    u=[117]
+    v=[118]
+    w=[119]
+    x=[120]
+    y=[121]
+    z=[122]
     ENTER=[10]
     
 
@@ -63,5 +123,5 @@ def is_key(char_list,key):
         if char_list[i:i+len(key)]==key:
             return(True)
     return(False)
-        
-        
+
+

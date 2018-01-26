a52917cac572219f5043331e05f3501f432a7f88
Javier Peralta <javinachop@gmail.com>
2018-01-26 11:10:38 -0600

    chg: dev: Modify yarp_tools module to comply with PEP8

--- lib/arcospyu/yarp_tools/__init__.py
+++ lib/arcospyu/yarp_tools/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import yarp_comm_helpers
 
-
+__all__ = ['yarp_comm_helpers']
 def main():
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()

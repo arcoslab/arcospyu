953afeb0d0d8147490c03cb5caae8919719d52ff
Javier Peralta <javinachop@gmail.com>
2018-01-26 10:46:37 -0600

    chg: dev: Modify pmanager module to comply with PEP8

--- lib/arcospyu/pmanager/__init__.py
+++ lib/arcospyu/pmanager/__init__.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from pmanager import PManager
+
+__all__ = ['PManager']
+
 
 def main():
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- lib/arcospyu/pmanager/pmanager.py
+++ lib/arcospyu/pmanager/pmanager.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Copyright (c) 2013 Federico Ruiz-Ugalde
 # Author: Federico Ruiz Ugalde <memeruiz at gmail.com>
 #
@@ -18,10 +19,13 @@
 from arcospyu.mypopen import MyPopen
 from arcospyu.dprint import iprint, dprint, eprint
 from time import sleep
-import signal, os
+import signal
+import os
+
 
 def which(program):
     import os
+
     def is_exe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
@@ -30,48 +34,54 @@ def which(program):
         if is_exe(program):
             return program
     else:
-        for path in os.environ["PATH"].split(os.pathsep):
+        for path in os.environ['PATH'].split(os.pathsep):
             path = path.strip('"')
             exe_file = os.path.join(path, program)
             if is_exe(exe_file):
                 return exe_file
     return None
 
+
 class PManager(object):
     def __init__(self, processes_args=[]):
-        self.processes_args=processes_args
-        self.processes=[]
-        self.processes_name_pos=[]
+        self.processes_args = processes_args
+        self.processes = []
+        self.processes_name_pos = []
 
     def start(self):
         for process_args in self.processes_args:
-            filename=which(process_args[0])
-            if filename and os.path.exists(filename) and os.access(filename, os.X_OK):
-                dprint("Starting process: ", process_args[0])
-                self.processes.append(MyPopen([filename]+process_args[1:]))
-                self.processes[-1].args=process_args
+            filename = which(process_args[0])
+            if filename and os.path.exists(filename) and os.access(filename,
+                                                                   os.X_OK):
+                dprint('Starting process: ', process_args[0])
+                self.processes.append(MyPopen([filename] + process_args[1:]))
+                self.processes[-1].args = process_args
             else:
-                eprint("Executable", process_args[0], " not found, not starting process")
-            #sleep(3)
+                eprint(
+                    'Executable', process_args[0],
+                    ' not found, not starting process')
+            # sleep(3)
 
     def monitor(self):
-        stop=False
+        stop = False
         while not stop:
             for process in self.processes:
                 process.poll()
-                if process.returncode != None:
-                    eprint("Process: ", process.args[0], " died! Closing all processes")
-                    stop=True
+                if process.returncode is not None:
+                    eprint(
+                        'Process: ', process.args[0],
+                        ' died! Closing all processes')
+                    stop = True
             sleep(0.5)
-            #dprint("Looping")
+            # dprint('Looping')
         if stop:
             self.stop()
 
     def stop(self, sec=10):
-        iprint("Sending signal ", signal.SIGTERM, " to subprocesses")
+        iprint('Sending signal ', signal.SIGTERM, ' to subprocesses')
         [process.send_signal2(signal.SIGTERM) for process in self.processes]
-        iprint("Waiting processes to terminate")
+        iprint('Waiting processes to terminate')
         for process in self.processes:
             process.wait_and_kill(sec)
-            iprint("Process: ", process.args[0], " terminated")
-        iprint("All processes terminated!")
+            iprint('Process: ', process.args[0], ' terminated')
+        iprint('All processes terminated!')

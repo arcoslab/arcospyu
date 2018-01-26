f649dbcbd86cabc4e6f33136d47a2a8e444bb3f7
Javier Peralta <javinachop@gmail.com>
2018-01-25 20:55:48 -0600

    chg: dev: Update setup.py
    The setup.py was modified to include dependencies from a requierments.txt, adds
    more metadata and does dynamic module searching.

--- lib/arcospyu/__init__.py
+++ lib/arcospyu/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import computer_graphics, config_parser, control, dprint, kdl_helpers, mypopen, numeric, print_colors, rawkey, pmanager, robot_tools, yarp_tools, signal_proc
 
+__version__ = '0.1'
+
 
 def main():
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()

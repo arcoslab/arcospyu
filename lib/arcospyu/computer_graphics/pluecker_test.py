f8277d3a0463a4bf3dac23dad73b6723cd137198
Javier Peralta <javinachop@gmail.com>
2018-01-25 21:02:11 -0600

    chg: dev: Modify computer_graphics module to comply with PEP8

--- lib/arcospyu/computer_graphics/pluecker_test.py
+++ lib/arcospyu/computer_graphics/pluecker_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Copyright (c) 2009 Technische Universitaet Muenchen, Informatik Lehrstuhl IX.
 # Author: Federico Ruiz-Ugalde <ruizf at in.tum.de>
 #
@@ -15,80 +16,96 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#Some theory about this can be found here:
-#http://www.cgafaq.info/wiki/Plucker_Coordinates also on the wikipedia of course
+# Some theory about this can be found here:
+# http://www.cgafaq.info/wiki/Plucker_Coordinates also on the wikipedia of
+# course
 
 import numpy as n
 
-def rayIsParallelNormal(vertexA,vertexB,vertexC,rayD,epsilon=1e-10):
-  N=n.cross(vertexB-vertexA,vertexC-vertexA)
-  test=n.cross(rayD,N)
-  test2=n.inner(test,test)
-  if (test2>-epsilon and test2<epsilon):
-    return 0 #ray and normal are parallel
-  else:
-    return 1 #are not parallel
 
-def plane_ray_intersection(plane_normal,point_in_plane,ray_pos,ray_dir):
-  u2=n.cross(ray_dir,ray_pos)
-  u1=ray_dir
-  d=n.dot(plane_normal,point_in_plane)
-  return((n.cross(u2,plane_normal)-d*u1)/n.dot(u1,plane_normal))
-
-def triangle_ray_intersection(vertexA,vertexB,vertexC,rayPos,rayD):
-  e1=n.array(vertexB-vertexA)
-  e2=n.array(vertexC-vertexA)
-  T=n.array(rayPos-vertexA)
-  P=n.array(n.cross(rayD,e2))
-  Q=n.array(n.cross(T,e1))
-  X=(1.0/(n.inner(P,e1)))*n.array([n.inner(Q,e2),n.inner(P,T),n.inner(Q,rayD)])
-  return X	#t,u,v
-
-def trianglePlueckerTest(vertexA,vertexB,vertexC,rayPos,rayD,epsilon=1e-10):
-  e0=n.array((vertexA-vertexB).tolist()+(n.cross(vertexA,vertexB)).tolist())
-  e1=n.array((vertexC-vertexA).tolist()+(n.cross(vertexC,vertexA)).tolist())
-  e2=n.array((vertexB-vertexC).tolist()+(n.cross(vertexB,vertexC)).tolist())
-  R=n.array([(n.cross(rayD,rayPos)).tolist()+rayD.tolist()])
-  t0=n.inner(e0,R)
-  t1=n.inner(e1,R)
-  t2=n.inner(e2,R)
-  print "t0: ",t0 #If zero, means is touching line B-A
-  print "t1: ",t1 #If zero, means, is touching line C-A
-  print "t2: ",t2 #If zero, measn, is touching line C-B
-  #if the signs are all the same it means the ray passes through the triangle
-  #if any t0 is 0 or near 0 it means the ray passes throught the line or is parallel to the line (passes
-  #through the line in infinity)
-  if ((t0>0 and t1>0 and t2>0) or (t0<0 and t1<0 and t2<0)):
-    print "Inside"
-    return 1
-  elif ((t0>-epsilon and t0<epsilon) or (t1>-epsilon and t1<epsilon) or (t2>-epsilon and t2<epsilon)):
-    print "On line"
-    return 0
-  else:
-    print "Outside"
-    return -1
-
-def polygon_pluecker_test(vertex_list,ray_pos,ray_dir,epsilon=1e-10):
-  pluecker_list=[]
-  for i,vertexi in enumerate(vertex_list):
-    if i<len(vertex_list)-1:
-      vertexj=vertex_list[i+1]
+def rayIsParallelNormal(vertexA, vertexB, vertexC, rayD, epsilon=1e-10):
+    N = n.cross(vertexB - vertexA, vertexC - vertexA)
+    test = n.cross(rayD, N)
+    test2 = n.inner(test, test)
+    if (test2 > -epsilon and test2 < epsilon):
+        return 0  # ray and normal are parallel
     else:
-      vertexj=vertex_list[0]
-    pluecker_list.append(n.concatenate((vertexj-vertexi,n.cross(vertexj,vertexi))))
-  R=n.concatenate((n.cross(ray_dir,ray_pos),ray_dir))
-  ts=[]
-  for pluecker in pluecker_list:
-    ts.append(n.inner(pluecker,R))
-  return(ts)
-    
-  
-#A=n.array([0.1,0.1,0.1])
-#B=n.array([0.1,0.3,0.1])
-#C=n.array([0.1,0.3,0.3])
-#rayPos=n.array([0.4,0.22,0.2])
-#rayD=n.array([-1,0,0])
-#segments=visual.curve(pos=[A,B,C,A],radius=0.01)
-#ray=visual.arrow(pos=rayPos,axis=rayD,shaftwidth=0.003,headwidth=0.007,fixedwidth=1)
-#t.trianglePlueckerTest(A,B,C,rayPos,rayD)
-#ray.pos=rayPos
+        return 1  # are not parallel
+
+
+def plane_ray_intersection(plane_normal, point_in_plane, ray_pos, ray_dir):
+    u2 = n.cross(ray_dir, ray_pos)
+    u1 = ray_dir
+    d = n.dot(plane_normal, point_in_plane)
+    return ((n.cross(u2, plane_normal) - d * u1) / n.dot(u1, plane_normal))
+
+
+def triangle_ray_intersection(vertexA, vertexB, vertexC, rayPos, rayD):
+    e1 = n.array(vertexB - vertexA)
+    e2 = n.array(vertexC - vertexA)
+    T = n.array(rayPos - vertexA)
+    P = n.array(n.cross(rayD, e2))
+    Q = n.array(n.cross(T, e1))
+    X = (1.0 / (n.inner(P, e1))) * n.array(
+        [n.inner(Q, e2), n.inner(P, T),
+         n.inner(Q, rayD)])
+    return X  # t,u,v
+
+
+def trianglePlueckerTest(
+        vertexA, vertexB, vertexC, rayPos, rayD, epsilon=1e-10):
+    e0 = n.array(
+        (vertexA - vertexB).tolist() + (n.cross(vertexA, vertexB)).tolist())
+    e1 = n.array(
+        (vertexC - vertexA).tolist() + (n.cross(vertexC, vertexA)).tolist())
+    e2 = n.array(
+        (vertexB - vertexC).tolist() + (n.cross(vertexB, vertexC)).tolist())
+    R = n.array([(n.cross(rayD, rayPos)).tolist() + rayD.tolist()])
+    t0 = n.inner(e0, R)
+    t1 = n.inner(e1, R)
+    t2 = n.inner(e2, R)
+    print 't0: ', t0  # If zero, means is touching line B-A
+    print 't1: ', t1  # If zero, means, is touching line C-A
+    print 't2: ', t2  # If zero, measn, is touching line C-B
+
+    # if the signs are all the same it means the ray passes through the
+    # triangle if any t0 is 0 or near 0 it means the ray passes throught the
+    # line or is parallel to the line (passes through the line in infinity)
+
+    if ((t0 > 0 and t1 > 0 and t2 > 0) or (t0 < 0 and t1 < 0 and t2 < 0)):
+        print 'Inside'
+        return 1
+    elif ((t0 > -epsilon and t0 < epsilon) or (t1 > -epsilon and t1 < epsilon)
+          or (t2 > -epsilon and t2 < epsilon)):
+        print 'On line'
+        return 0
+    else:
+        print 'Outside'
+        return -1
+
+
+def polygon_pluecker_test(vertex_list, ray_pos, ray_dir, epsilon=1e-10):
+    pluecker_list = []
+    for i, vertexi in enumerate(vertex_list):
+        if i < len(vertex_list) - 1:
+            vertexj = vertex_list[i + 1]
+        else:
+            vertexj = vertex_list[0]
+        pluecker_list.append(
+            n.concatenate((vertexj - vertexi, n.cross(vertexj, vertexi))))
+    R = n.concatenate((n.cross(ray_dir, ray_pos), ray_dir))
+    ts = []
+    for pluecker in pluecker_list:
+        ts.append(n.inner(pluecker, R))
+    return (ts)
+
+
+# A=n.array([0.1,0.1,0.1])
+# B=n.array([0.1,0.3,0.1])
+# C=n.array([0.1,0.3,0.3])
+# rayPos=n.array([0.4,0.22,0.2])
+# rayD=n.array([-1,0,0])
+# segments=visual.curve(pos=[A,B,C,A],radius=0.01)
+# ray=visual.arrow(pos=rayPos,axis=rayD,shaftwidth=0.003,headwidth=0.007,fixedwidth=1)
+# t.trianglePlueckerTest(A,B,C,rayPos,rayD)
+# ray.pos=rayPos
--- lib/arcospyu/computer_graphics/ray_tracing.py
+++ lib/arcospyu/computer_graphics/ray_tracing.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Copyright (c) 2013 Federico Ruiz Ugalde
 # Author: Federico Ruiz Ugalde <memeruiz at gmail.com>
 #
@@ -17,90 +18,95 @@
 
 from numpy import dot, array
 from numpy.linalg import norm
-from arcospyu.computer_graphics.pluecker_test import polygon_pluecker_test, plane_ray_intersection
+from arcospyu.computer_graphics.pluecker_test import (
+    polygon_pluecker_test, plane_ray_intersection)
 from arcospyu.robot_tools.robot_trans import homo_matrix
 
 
-def plane_ray_intersec2(normal_plane,point_plane,vector,pos):
-    d=dot((point_plane-pos),normal_plane)/dot(vector,normal_plane)
-    result=pos+vector*d
-    return([d,result])
+def plane_ray_intersec2(normal_plane, point_plane, vector, pos):
+    d = dot((point_plane - pos), normal_plane) / dot(vector, normal_plane)
+    result = pos + vector * d
+    return ([d, result])
 
 
-def most_far_away_face_from_point(crossing_faces_local, crossing_faces_reference_frame,  point):
-    dist=-1
-    final_face=-1
+def most_far_away_face_from_point(
+        crossing_faces_local, crossing_faces_reference_frame, point):
+    dist = -1
+    final_face = -1
     for face in crossing_faces_local:
-        touch_point_local=crossing_faces_local[face][2][1]
-        touch_point=dot(crossing_faces_reference_frame,homo_matrix(trans=touch_point_local))[:3,3]
-        print "Touch point", face, touch_point
-        cur_dist=norm(touch_point-point)
-        if dist<0.:
-            dist=cur_dist
-            final_touch_point=touch_point
-            final_face=face
+        touch_point_local = crossing_faces_local[face][2][1]
+        touch_point = dot(
+            crossing_faces_reference_frame,
+            homo_matrix(trans=touch_point_local))[:3, 3]
+        print 'Touch point', face, touch_point
+        cur_dist = norm(touch_point - point)
+        if dist < 0.:
+            dist = cur_dist
+            final_touch_point = touch_point
+            final_face = face
         else:
-            if cur_dist>dist:
-                dist=cur_dist
-                final_touch_point=touch_point
-                final_face=face
-    return(final_touch_point,final_face)
+            if cur_dist > dist:
+                dist = cur_dist
+                final_touch_point = touch_point
+                final_face = face
+    return (final_touch_point, final_face)
 
 
-def find_crossing_faces(vector,vector_pos, vertices_faces, planes):
-    crossing_faces={}
+def find_crossing_faces(vector, vector_pos, vertices_faces, planes):
+    crossing_faces = {}
     for face in vertices_faces:
-        temp=array(polygon_pluecker_test(vertices_faces[face],
-                                        vector_pos,vector/norm(vector)))
-#        print "pluecker test", face, temp
-        crossing_face=False
-        if all(temp>0.) or all(temp<0.):
-#            print "Finger will cross face:", face
-            crossing_face=True
-        elif all(temp>=0.) or all(temp<=0.):
-#            print "not positive"
-            #if the zeros are only two and are next to each other then it
-            #crosses the face
-            i=0
-            j=0
-            while i<len(temp)-j:
-                num=temp[i]
-#                print "num", num
-                if num==0. and not crossing_face:
-#                    print "zero"
-                    crossing_face=True #we suppose beforehand
-                    i+=1
-                    if i==len(temp):
-                        num=temp[0]
+        temp = array(
+            polygon_pluecker_test(
+                vertices_faces[face], vector_pos, vector / norm(vector)))
+        #        print 'pluecker test', face, temp
+        crossing_face = False
+        if all(temp > 0.) or all(temp < 0.):
+            #            print 'Finger will cross face:', face
+            crossing_face = True
+        elif all(temp >= 0.) or all(temp <= 0.):
+            #            print 'not positive'
+            # if the zeros are only two and are next to each other then it
+            # crosses the face
+            i = 0
+            j = 0
+            while i < len(temp) - j:
+                num = temp[i]
+                #                print 'num', num
+                if num == 0. and not crossing_face:
+                    #                    print 'zero'
+                    crossing_face = True  # we suppose beforehand
+                    i += 1
+                    if i == len(temp):
+                        num = temp[0]
                     else:
-                        num=temp[i]
-                    i+=1
-                    if num==0.:
- #                       print "crossing face, two zeros"
-                         pass
+                        num = temp[i]
+                    i += 1
+                    if num == 0.:
+                        # print 'crossing face, two zeros'
+                        pass
                     else:
- #                       print "crossing face, one zero"
-                        if i==2:
- #                           print "Don't check for last item"
-                            j=1
-                elif num==0. and crossing_face:
-                    crossing_face=False #we rectify wrong previews assumption
+                        # print 'crossing face, one zero'
+                        if i == 2:
+                            # print 'Don't check for last item'
+                            j = 1
+                elif num == 0. and crossing_face:
+                    crossing_face = False  # we rectify wrong previews assumption # noqa
                     break
                 else:
-                    i+=1
+                    i += 1
         if crossing_face:
-            #calculate where is crossing the face
-            vertices=vertices_faces[face]
-            #intersection=triangle_ray_intersection(vertices[0],vertices[1],
+            # calculate where is crossing the face
+            vertices = vertices_faces[face]
+            # intersection=triangle_ray_intersection(vertices[0],vertices[1],
             #                                       vertices[2],vector_pos,vector)
-            #intersection=plane_ray_intersection(array(planes[face][0]),
+            # intersection=plane_ray_intersection(array(planes[face][0]),
             #                                    array(planes[face][1]),
             #                                    vector_pos,vector)
-            intersection=plane_ray_intersec2( array(planes[face][0]),
-                                              array(planes[face][1]),
-                                              vector,vector_pos)
-            intersection[0]*=norm(vector)
-            print "intersection", intersection
-            crossing_faces[face]=[vertices_faces[face],temp,intersection]
-    print "crossing_faces", crossing_faces
-    return(crossing_faces)
+            intersection = plane_ray_intersec2(
+                array(planes[face][0]), array(planes[face][1]), vector,
+                vector_pos)
+            intersection[0] *= norm(vector)
+            print 'intersection', intersection
+            crossing_faces[face] = [vertices_faces[face], temp, intersection]
+    print 'crossing_faces', crossing_faces
+    return (crossing_faces)

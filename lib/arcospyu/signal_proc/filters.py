0fcffc17be8202bebb532c0a6715accc293d0b7a
Javier Peralta <javinachop@gmail.com>
2018-01-26 11:02:12 -0600

    chg: dev: Modify signal_proc module to comply with PEP8

--- lib/arcospyu/signal_proc/filters.py
+++ lib/arcospyu/signal_proc/filters.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Copyright (c) 2013 Federico Ruiz Ugalde
 # Author: Federico Ruiz Ugalde <memeruiz at gmail.com>
 #
@@ -21,30 +22,35 @@ from numpy import array
 
 class Filter_vector():
     def __init__(self, dim=3, order=2, freq=0.7, y=[], x=[]):
-        self.dim=dim
-        self.filters=[]
+        self.dim = dim
+        self.filters = []
         for i in xrange(dim):
-            if len(y)>0:
-                self.filters.append(Filter(order=order, freq=freq, y=y[i], x=x[i]))
+            if len(y) > 0:
+                self.filters.append(
+                    Filter(order=order, freq=freq, y=y[i], x=x[i]))
             else:
                 self.filters.append(Filter(order=order, freq=freq))
 
     def filter(self, data):
-        return(array([self.filters[i].filter(array([data[i]]))[0] for i in xrange(self.dim)]))
+        return (
+            array(
+                [
+                    self.filters[i].filter(array([data[i]]))[0]
+                    for i in xrange(self.dim)
+                ]))
 
 
 class Filter():
-    def __init__(self, order=2, freq=0.7,y=[],x=[]):
-         self.b,self.a=iirfilter(order, freq, btype="lowpass")
-         if len(y)>0:
-             print "here"
-             self.z=lfiltic(self.b,self.a, y)
-         else:
-             self.z=array([0.]*order)
-         self.z=lfiltic(self.b,self.a, y,x=x)
-    
-    def filter(self,raw_data):
-        print "Raw", array(raw_data)
-        y,self.z=lfilter(self.b,self.a,raw_data,zi=self.z, axis=0)
-        return(y)
+    def __init__(self, order=2, freq=0.7, y=[], x=[]):
+        self.b, self.a = iirfilter(order, freq, btype='lowpass')
+        if len(y) > 0:
+            print 'here'
+            self.z = lfiltic(self.b, self.a, y)
+        else:
+            self.z = array([0.] * order)
+        self.z = lfiltic(self.b, self.a, y, x=x)
 
+    def filter(self, raw_data):
+        print 'Raw', array(raw_data)
+        y, self.z = lfilter(self.b, self.a, raw_data, zi=self.z, axis=0)
+        return (y)
